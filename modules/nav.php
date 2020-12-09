9b9b52a9ed1b726fab2b0d84ec6ad45ce1174bc7
Niraj Patil <72375928+niraj2347@users.noreply.github.com>
2020-12-09 20:13:14 +0530

    Dashboard added

---
+++ modules/nav.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+?>
+
+<?php if (isset($_SESSION['islogin']) && $_SESSION['islogin'] == 1) : ?>
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+			<ul class="nav sidebar-nav">
+					<li class="sidebar-brand">
+
+							<a href="index.php">
+						 <!-- TODO: add image -->
+								<!-- <img src="images/mattendance_logo_small.png" alt="mAttendance"> -->
+							</a>
+					</li>
+					<li>
+							<a href="index.php?page=dashboard">Dashboard</a>
+					</li>
+					<li>
+							<a href="index.php">Take Attendance</a>
+					</li>
+					<li>
+							<a href="index.php?page=studentinfo">List of Students</a>
+					</li>
+					<li>
+							<a href="index.php?page=reports">Reports</a>
+					</li>
+					<li>
+							<a href="index.php?page=help">Help</a>
+					</li>
+					<li>
+							<a href="index.php?page=logout">Logout</a>
+					</li>
+			</ul>
+	</nav>
+<?php else: ?>
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+			<ul class="nav sidebar-nav">
+					<li class="sidebar-brand">
+							<a href="index.php">
+								<!-- <img src="images/mattendance_logo_small.png" alt="mAttendance"> -->
+							</a>
+					</li>
+					<li>
+							<a href="index.php">Login</a>
+					</li>
+					<li>
+							<a href="index.php">Teacher's Section</a>
+					</li>
+					<li>
+							<a href="index.php">Student's Section</a>
+					</li>
+			</ul>
+	</nav>
+<?php endif; ?>
